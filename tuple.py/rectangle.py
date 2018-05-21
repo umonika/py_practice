36334edb343903ad019415f0b2c5b27d23481cc5
umonika <39208561+umonika@users.noreply.github.com>
2018-05-21 20:35:42 +0530

    add a file

--- tuple.py/rectangle.py
+++ tuple.py/rectangle.py
@@ -4,5 +4,8 @@ class rectangle:
         self.b = b
     def area(self):
         return self.l * self.b
+    def peri(self):
+        return 2 * (self.l + self.b)    
 r1 = rectangle(4,6)
 print("area ",r1.area())
+print("perimeter :",r1.peri())
